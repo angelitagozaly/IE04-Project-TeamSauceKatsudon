3315a6ab881826e6279c3f5a8124ce9f31b5e604
angelitagozaly <cangelitag@gmail.com>
2023-01-11 00:10:05 +0900

    Added views and user authorization for login page

--- src/ta_report_system/tareportsystem/urls.py
+++ src/ta_report_system/tareportsystem/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('student-login', views.studentlogin, name='studentlogin'),
+    path('lecturer-login', views.lecturerlogin, name='lecturerlogin'),
     path('student-main', views.studentmain, name='studentmain'),
+    path('lecturer-main', views.lecturermain, name='lecturermain'),
+    path('student-calendar', views.studentcalendar, name='studentcalendar'),
 ]
--- src/ta_report_system/tareportsystem/views.py
+++ src/ta_report_system/tareportsystem/views.py
@@ -1,13 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
 from django.http import HttpResponse
+from tareportsystem import models as tareportsystem_models
+from django.contrib import messages
 
 # Create your views here.
 
 def studentlogin(request):
-    template = loader.get_template('tareportsystem/studentlogin.html')
-    return HttpResponse(template.render())
+    return render(request, 'tareportsystem/studentlogin.html')
+
+def lecturerlogin(request):
+    template = loader.get_template('tareportsystem/lecturerlogin.html')
+    return render(request, 'tareportsystem/lecturerlogin.html')
 
 def studentmain(request):
-    template = loader.get_template('tareportsystem/studentmain.html')
+    uname = request.POST['Username']
+    passw = request.POST['Password']
+    if tareportsystem_models.Student.objects.filter(Username = uname, Password = passw).exists():
+        template = loader.get_template('tareportsystem/studentmain.html')
+        return HttpResponse(template.render())
+    else:
+        messages.error(request,'Username or password is not correct')
+        return redirect('studentlogin')
+
+def lecturermain(request):
+    uname = request.POST['Username']
+    passw = request.POST['Password']
+    if tareportsystem_models.Lecturer.objects.filter(Username = uname, Password = passw).exists():
+        template = loader.get_template('tareportsystem/lecturermain.html')
+        return HttpResponse(template.render())
+    else:
+        messages.error(request,'Username or password is not correct')
+        return redirect('lecturerlogin')
+
+def studentcalendar(request):
+    template = loader.get_template('tareportsystem/studentcalendar.html')
     return HttpResponse(template.render())
